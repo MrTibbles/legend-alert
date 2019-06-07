af0baaa9c1aeed6cbfc690bda3eb3aa60a8c3c58
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-06-07 17:09:32 +0100

    tools: Add webpack deps, config and start script

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env = {}) => {
+  const isInDev = env.production !== true
+
+  return {
+    entry: path.resolve(__dirname, 'src'),
+    mode: isInDev ? 'development' : 'production',
+    devtool: isInDev ? 'eval' : 'source-map',
+    devServer: {
+      hot: true,
+      contentBase: path.join(__dirname, 'dist'),
+      port: 3000,
+      stats: 'errors-warnings'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'legend-alert.[name].[hash].js'
+    },
+    plugins: [
+      new webpack.DefinePlugin({ foo: 'bar' }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: '🚨'
+      }),
+    ],
+  }
+}
