78945acae69675b4599a867aa6bcc64135c947c1
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-06-09 18:01:01 +0100

    Add linaria and tmp corejs version to handle es6.regexp.to-string issue

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env = {}) => {
   const isInDev = env.production !== true
@@ -22,6 +23,38 @@ module.exports = (env = {}) => {
       publicPath: '/',
       filename: 'legend-alert.[name].[hash].js'
     },
+    module: {
+     rules: [
+       {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         use: [
+           {
+             loader: 'babel-loader',
+           },
+          {
+            loader: 'linaria/loader',
+            options: {
+              sourceMap: isInDev,
+            },
+          }
+        ]
+      },
+      {
+       test: /\.css$/,
+       use: [
+         {
+           loader: MiniCssExtractPlugin.loader,
+           options: {
+             sourceMap: isInDev,
+             hmr: isInDev
+           },
+         },
+         'css-loader'
+       ],
+      },
+     ]
+    },
     plugins: [
       new webpack.DefinePlugin({ foo: 'bar' }),
       new CleanWebpackPlugin(),
@@ -31,6 +64,9 @@ module.exports = (env = {}) => {
           viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
         }
       }),
+      new MiniCssExtractPlugin({
+        filename: 'linaria-styles.css',
+      })
     ],
   }
 }
