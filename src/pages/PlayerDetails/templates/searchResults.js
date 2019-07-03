0b574e1d1f63623c7fae694cf9526431bda0582a
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-03 10:03:40 +0100

    Render search results to ul - lofi

--- src/pages/PlayerDetails/templates/searchResults.js
+++ src/pages/PlayerDetails/templates/searchResults.js
@@ -1,9 +1,20 @@
 import { html } from "lit-html";
 
-// import * as styles from '../styles'
+import { searchResultsContainer, searchResultsList } from '../styles'
 
 const searchResultsMarkup = (results = []) => html`
-  <p>${results.length}</p>
+  <div class=${searchResultsContainer}>
+    <h3>The following players matched your search, select one to see how much of a legend they are:</h3>
+    <ul class=${searchResultsList}>
+      ${results.map(({ platformSlug, platformUserHandle }) =>
+        html`
+          <li @click=${() => console.info('LEGEND', platformUserHandle)}>
+            <h3><span class="highlight">${platformUserHandle}</span> on <span class="highlight uppercase">${platformSlug}</span></h3>
+          </li>
+        `
+      )}
+    </ul>
+  </div>
 `
 
 export default searchResultsMarkup
