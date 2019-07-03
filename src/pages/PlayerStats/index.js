e9ceb9dd63c6e732121c0fbf3883123781039fbd
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-03 19:40:04 +0100

    Add fetchPlayerStats method with basic functionality

--- src/pages/PlayerStats/index.js
+++ src/pages/PlayerStats/index.js
@@ -1,11 +1,50 @@
 import { html, render } from "lit-html";
 
-function renderPlayerStatsView(playerHandle) {
+function renderPlayerStatsView({
+  activeLegend
+}) {
 
   return render(
-    html`<p>${playerHandle.platformUserHandle} Stats</p>`,
+    html`<p>${activeLegend.platformUserHandle} Stats</p>`,
     document.body
   )
 }
 
-export default renderPlayerStatsView
+const fetchPlayerStats = async (platformSlug, platformUserHandle) => {
+  console.info('loading')
+
+  const { data } = await fetch(
+    `/apex-api/v1/apex/standard/profile/${platformSlug}/${platformUserHandle}`,
+    {
+      credentials: 'omit',
+      headers: {
+        'TRN-Api-Key': TRN_TOKEN,
+      },
+      mode: 'cors',
+  })
+  .catch(err => {
+    console.warn(err)
+  })
+  .then(res => {
+    if (!res.ok) {
+      console.warn('Something went wron')
+    }
+
+    return res.json()
+  })
+
+  return data
+}
+
+const PlayerStats = async activeLegend => {
+
+  const stats = await fetchPlayerStats(
+    activeLegend.platformSlug, activeLegend.platformUserHandle
+  )
+
+  console.info(stats)
+
+  return renderPlayerStatsView({ activeLegend })
+}
+
+export default PlayerStats
