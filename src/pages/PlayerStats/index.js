30f5e7568fcf94306163f64ffefd2805feb9ccda
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-05 23:11:16 +0100

    Change to player-stats-view custom WC approach - state management needs improving

--- src/pages/PlayerStats/index.js
+++ src/pages/PlayerStats/index.js
@@ -3,60 +3,110 @@ import { html, render } from "lit-html";
 import * as utils from "./utils";
 import * as templates from "./templates";
 
-function renderPlayerStatsView({ activeLegend, activePlayer }) {
-  const activeLegendIconImage = utils.getLegendIconImage(activeLegend);
+class PlayerStats extends HTMLElement {
+  constructor() {
+    super();
 
-  return render(
-    html`
-      ${templates.heroSection({
-        image: activeLegendIconImage,
-        playerHandle: activePlayer.platformUserHandle,
-        playerPlatform: activePlayer.platformSlug
-      })}
-      ${templates.statsGrid({ stats: activeLegend.stats })}
-    `,
-    document.body
-  );
+    this.state = {
+      activeLegend: undefined,
+      activePlayer: undefined,
+      fetchState: { data: undefined, error: undefined, loading: false }
+    };
+  }
+
+  async fetchPlayerStats(platformSlug, platformUserHandle) {
+    this.state = {
+      ...this.state,
+      fetchState: {
+        ...this.state.fetchState,
+        loading: true
+      }
+    };
+
+    this._render();
+
+    const { data } = await fetch(
+      `/apex-api/v1/apex/standard/profile/${platformSlug}/${platformUserHandle}`,
+      {
+        credentials: "omit",
+        headers: {
+          "TRN-Api-Key": TRN_TOKEN
+        },
+        mode: "cors"
+      }
+    )
+      .catch(err => {
+        console.warn(err);
+      })
+      .then(res => {
+        if (!res.ok) {
+          console.warn("Something went wrong");
+        }
+
+        return res.json();
+      });
+
+    // the below is ridiculous
+    // need state machine
+    this.state = {
+      ...this.state,
+      activeLegend: !this.state.activeLegend
+        ? utils.getActiveLegend(data.children)
+        : this.state.activeLegend,
+      fetchState: {
+        data,
+        error: undefined,
+        loading: false
+      }
+    };
+
+    return this._render();
+  }
+
+  connectedCallback() {
+    if (this.isConnected) {
+      document.body.classList.add("dark");
+
+      this.state = {
+        ...this.state,
+        ...this.initialState
+      };
+
+      const {
+        activePlayer: { platformSlug, platformUserHandle }
+      } = this.state;
+
+      return this.fetchPlayerStats(platformSlug, platformUserHandle);
+    }
+  }
+
+  _render() {
+    const {
+      activeLegend,
+      activePlayer,
+      fetchState: { error, loading }
+    } = this.state;
+
+    if (loading) return console.info("loading");
+
+    if (error) return console.warn("something went wrong");
+
+    const activeLegendIconImage = utils.getLegendIconImage(activeLegend);
+
+    return render(
+      html`
+        ${templates.heroSection({
+          image: activeLegendIconImage,
+          playerHandle: activePlayer.platformUserHandle,
+          playerPlatform: activePlayer.platformSlug
+        })}
+        ${templates.statsGrid({ stats: activeLegend.stats })}
+      `,
+      document.querySelector("player-stats-view")
+    );
+  }
 }
 
-const fetchPlayerStats = async (platformSlug, platformUserHandle) => {
-  console.info("loading");
-
-  const { data } = await fetch(
-    `/apex-api/v1/apex/standard/profile/${platformSlug}/${platformUserHandle}`,
-    {
-      credentials: "omit",
-      headers: {
-        "TRN-Api-Key": TRN_TOKEN
-      },
-      mode: "cors"
-    }
-  )
-    .catch(err => {
-      console.warn(err);
-    })
-    .then(res => {
-      if (!res.ok) {
-        console.warn("Something went wrong");
-      }
-
-      return res.json();
-    });
-
-  return data;
-};
-
-const PlayerStats = async activePlayer => {
-  document.body.classList.add("dark");
-
-  const stats = await fetchPlayerStats(
-    activePlayer.platformSlug,
-    activePlayer.platformUserHandle
-  );
-
-  const activeLegend = utils.getActiveLegend(stats.children);
-
-  return renderPlayerStatsView({ activeLegend, activePlayer });
-};
+customElements.define("player-stats-view", PlayerStats);
 
 export default PlayerStats;
