07d2b81137f668bab47dc0a9befa9e719e04e937
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-07 11:49:51 +0100

    Add theme file for palette usage in JS and add state values for playerdetails form

--- src/pages/PlayerSearch/components/PlayerDetailsForm.js
+++ src/pages/PlayerSearch/components/PlayerDetailsForm.js
@@ -1,8 +1,19 @@
 import React from "react";
 import * as styles from "../styles";
+import palette from "../../../styles/theme";
 import { KeyboardIcon, PSIcon, XboxIcon } from "../../../components";
 
+/*
+  TODOS:
+    - Create Button component with default type attribute
+ */
+
 const PlayerDetailsForm = () => {
+  const [platformChoice, setPlatformChoice] = React.useState("");
+  const [playerHandle, setPlayerHandle] = React.useState("");
+
+  const onPlatformOptionSelected = option => setPlatformChoice(option);
+
   return (
     <section className="pane">
       <h2>Enter your player details below</h2>
@@ -12,29 +23,38 @@ const PlayerDetailsForm = () => {
           <div className={styles.platformChoices}>
             <div className="platform-opt">
               <button
+                onClick={() => onPlatformOptionSelected("psn")}
                 type="button"
-                // onClick={onPlatformOptionSelected}
-                // data-option="psn"
               >
-                <PSIcon />
+                <PSIcon
+                  color={
+                    platformChoice === "psn" ? palette.primary : palette.dark
+                  }
+                />
               </button>
             </div>
             <div className="platform-opt">
               <button
+                onClick={() => onPlatformOptionSelected("xbl")}
                 type="button"
-                // onClick={onPlatformOptionSelected}
-                // data-option="xbl"
               >
-                <XboxIcon />
+                <XboxIcon
+                  color={
+                    platformChoice === "xbl" ? palette.primary : palette.dark
+                  }
+                />
               </button>
             </div>
             <div className="platform-opt">
               <button
+                onClick={() => onPlatformOptionSelected("origin")}
                 type="button"
-                // onClick={onPlatformOptionSelected}
-                // data-option="origin"
               >
-                <KeyboardIcon />
+                <KeyboardIcon
+                  color={
+                    platformChoice === "origin" ? palette.primary : palette.dark
+                  }
+                />
               </button>
             </div>
           </div>
@@ -42,11 +62,15 @@ const PlayerDetailsForm = () => {
             <h3>Player tag</h3>
             <input
               name="player-tag"
+              onChange={({ target: { value } }) => setPlayerHandle(value)}
               placeholder="absolute-legend"
-              // value={''}
+              value={playerHandle}
             />
           </div>
         </div>
+        <button className={styles.confirmBtn} type="button">
+          Confirm
+        </button>
       </form>
     </section>
   );
