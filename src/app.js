843b08bb87002d01e671e87c5b0ce543ff514452
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-08 13:50:14 +0100

    Add ActivePlayer context and basic hook

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import localforage from "localforage";
-
-import * as Pages from "./pages";
-
+import localforage from "localforage";
 import "./styles/base.css";
 
+import { ActivePlayerProvider } from "./context/ActivePlayer";
+import * as Pages from "./pages";
+
+localforage.config({
+  description: "Apex Legends player stats tracker",
+  name: "legend-alert",
+  storeName: "players"
+});
+
 const App = () => (
-  <Router>
-    <Route component={Pages.PlayerSearch} exact path="/" />
-  </Router>
+  <ActivePlayerProvider>
+    <Router>
+      <Route component={Pages.PlayerSearch} exact path="/" />
+    </Router>
+  </ActivePlayerProvider>
 );
 
 ReactDom.render(<App />, document.getElementById("app-root"));
