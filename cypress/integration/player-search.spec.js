de9f468354358ac2907c585c744f8afd2dd1847a
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-09 16:11:03 +0100

    Add whatwg-fetch polyfil for stubbin network requests in cypress

--- cypress/integration/player-search.spec.js
+++ cypress/integration/player-search.spec.js
@@ -10,10 +10,10 @@ describe('Integration :: Player Search', () => {
 
     cy.get('button[data-testid="submit-btn"]').as('submitBtn')
 
-    cy.server()
+    cy.server({ delay: 750 })
     cy.route({
       method: 'GET',
-      url: '/apex-api/v2/apex/standard/search?platform=psn&query=leroyjenkins',
+      url: 'http://localhost:3000/apex-api/v2/apex/standard/search?platform=psn&query=leroyjenkins',
       response: { data: [] }
     }).as('searchLeroyJenkins')
   })
@@ -33,7 +33,7 @@ describe('Integration :: Player Search', () => {
   })
 
   it('Should not submit a player search with only a player handle entered', () => {
-    cy.get('@playerHandleInput').type('Leroy Jenkins')
+    cy.get('@playerHandleInput').type('leroyjenkins')
 
     cy.get('@submitBtn').click()
 
@@ -41,7 +41,7 @@ describe('Integration :: Player Search', () => {
   })
 
   it('Should submit a player search for Leroy Jenkins, and display no players found msg', () => {
-    cy.get('@playerHandleInput').type('Leroy Jenkins')
+    cy.get('@playerHandleInput').type('leroyjenkins')
 
     cy.get('@psnBtn').click()
 
