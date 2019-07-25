ec86fafac6c98a64ea02bc30057e6fef30e7864d
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-25 10:57:21 +0100

    Convert Container and ul to className styling on LegendSelector

--- src/pages/PlayerStats/components/LegendSelector.js
+++ src/pages/PlayerStats/components/LegendSelector.js
@@ -1,42 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ArrowIcon from "../../../ui-icons/ArrowIcon";
+import { css } from "linaria";
 import { styled } from "linaria/react";
 
-const Container = styled.aside`
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  background: var(--color-offwhite);
-  width: 25vw;
-  height: 100vh;
-  transform: translateX(-25vw);
+const styles = {
+  container: css`
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background: var(--color-offwhite);
+    width: 25vw;
+    height: 100vh;
+    transform: translateX(-25vw);
 
-  &:before {
-    content: "";
-    height: 900%; /* TO ENSURE BOTTOM EDGE IS NOT EXPOSED */
-    width: 110%;
-    box-shadow: 0 0 20px var(--color-primary) inset;
-    position: absolute;
-    right: 0px;
-    top: -5%;
-    pointer-events: none;
-  }
+    &:before {
+      content: "";
+      height: 900%; /* TO ENSURE BOTTOM EDGE IS NOT EXPOSED */
+      width: 110%;
+      box-shadow: 0 0 20px var(--color-primary) inset;
+      position: absolute;
+      right: 0px;
+      top: -5%;
+      pointer-events: none;
+    }
 
-  @media (min-width: 1024px) {
-    transform: translateX(0);
-    position: unset;
-  }
-`;
-
-const LegendList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-  list-style: none;
-`;
+    @media (min-width: 1024px) {
+      transform: translateX(0);
+      position: unset;
+    }
+  `,
+  legendList: css`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+  `
+};
 
 const LegendItem = styled.li`
   display: flex;
@@ -56,8 +59,8 @@ const LegendItem = styled.li`
 `;
 
 const LegendSelector = ({ activeLegendName, legends, onLegendSelected }) => (
-  <Container>
-    <LegendList>
+  <aside className={styles.container}>
+    <ul className={styles.legendList}>
       {legends.map(legend => (
         <LegendItem
           isActiveLegend={legend.legendName === activeLegendName}
@@ -68,8 +71,14 @@ const LegendSelector = ({ activeLegendName, legends, onLegendSelected }) => (
           <ArrowIcon direction="right" height="100%" width="0.55rem" />
         </LegendItem>
       ))}
-    </LegendList>
-  </Container>
+    </ul>
+  </aside>
 );
+
+LegendSelector.propTypes = {
+  activeLegendName: PropTypes.string.isRequired,
+  legends: PropTypes.array.isRequired,
+  onLegendSelected: PropTypes.func.isRequired
+};
 
 export default LegendSelector;
