d562567eb407e1b3e5c0ed8bf36f8597501eaf8b
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-08-14 17:08:02 +0100

    client: Convert ui-icons to typescript

--- src/client/ui-icons/SearchIcon.tsx
+++ src/client/ui-icons/SearchIcon.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
+import * as React from "react";
 
-const SearchIcon = ({ color = "#1D1D1D", ...props }) => (
+interface IconProps {
+  color?: string;
+}
+
+const SearchIcon = ({ color = "#1D1D1D", ...props } : IconProps) => (
   <svg viewBox="0 0 18 18" {...props}>
     <path
       d="M17.71 16.29l-5.11-5.11a7 7 0 1 0-1.42 1.42l5.11 5.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM2 7a5 5 0 1 1 10 0A5 5 0 0 1 2 7z"
@@ -10,9 +13,5 @@ const SearchIcon = ({ color = "#1D1D1D", ...props }) => (
     />
   </svg>
 );
-
-SearchIcon.propTypes = {
-  color: PropTypes.string
-};
 
 export default SearchIcon;
