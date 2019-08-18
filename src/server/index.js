b3428c23460259448dc080b67f8026048cd9aaa8
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-08-18 16:52:35 +0100

    server: Removes express integration from server in favour of Now handling static files

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,5 @@
-const path = require("path");
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-micro");
 const dotenv = require("dotenv");
-const helmet = require("helmet");
 
 const TrackerNetworkAPI = require("./TrackerNetworkAPI");
 const typeDefs = require("./schema");
@@ -36,26 +33,4 @@ const server = new ApolloServer({
   typeDefs
 });
 
-const app = express();
-
-app.use(helmet());
-server.applyMiddleware({ app });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
-}
-
-const port = process.env.PORT || 4000;
-app.listen({ port }, () =>
-  console.log(
-    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
-  )
-);
-
-app.get("*", (req, res) => {
-  const filepath = !req.url.includes(".")
-    ? "/public/index.html"
-    : req.url.split("?")[0]; // removes ?__WB_REVISION__=* suffix
-
-  res.sendFile(path.join(__dirname, filepath));
-});
+module.exports = server.createHandler({ path: "/graphql" });
